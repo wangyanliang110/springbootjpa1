2892e787ea5c2fc7931a6aa9d1cf594cb28ccc1d
wangyanliang110 <494090200@qq.com>
2018-04-10 22:51:55 +0800

    modify message

--- src/main/java/com/springbootjpa/Application.java
+++ src/main/java/com/springbootjpa/Application.java
@@ -1,13 +1,13 @@
 package com.springbootjpa;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(MysqlxDatatypes.Scalar.String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
